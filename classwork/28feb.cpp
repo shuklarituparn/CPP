0e40ad932e6ebeea82879f3e4f181b7b22098714
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-28 17:42:14 +0300

    added course plan in 28feb

--- classwork/28feb.cpp
+++ classwork/28feb.cpp
@@ -2,5 +2,27 @@
 // Created by rituparn on 28.02.23.
 //
 
+/*
+ *  Course plans
+ *
+ *  1. Templates: 2.5 lessons
+ *
+ *  2. Exceptions: 1.5 weeks
+ *
+ *  3. STL: 6 weeks
+ *
+ * ````3.1- Containers
+ *     3.2- Iterators
+ *     3.3 Allocators
+ *
+ * 4. Modern C++ : 3 weeks. MOve semantics, overloading of complex functions. writing libraries.
+ *
+ * 5. 2-3 Home assignments
+ *      1st home assignments on 1 and 2 after 4 weeks of this semester
+ *      2nd home assignment on the next 2 things. How to work with containers, how to work with iterators
+ *      Deadline of 2-3 weeks.
+ *
+ *
+ */
 #include <iostream>
 
