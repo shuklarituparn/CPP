0550ef0d2ae5a70c640e62287ed5810f1e846139
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-03-07 17:42:01 +0300

    added about template class

--- classwork/template_class.cpp
+++ classwork/template_class.cpp
@@ -4,5 +4,27 @@ Create a template class that compares two integers and tells which is bigger
 
 */
 
+#include <iostream>
 
+template <typename  T>// T is a type of variable which class will contain
+class S{
+    public:
+    T a_;
+    T b_;
+    S(T a, T b): a_(a), b_(b){};
+    T Max(){
+        if(a_>b_){
+            return a_;
+        }
+        else{return b_;}
+    };
+};
+
+int main()
+{
+	S<std::string> s("bc", "ade");
+    std::cout<<s.Max();
+
+	return 0;
+}
 
