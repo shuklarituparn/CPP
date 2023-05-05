e47477608b0cb443a108c67aa65186bc62c58fa9
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-05-05 19:51:14 +0300

    added comments

--- classwork/Algorithms-class/tasks/stack/stack_using_arrays/main.cpp
+++ classwork/Algorithms-class/tasks/stack/stack_using_arrays/main.cpp
@@ -3,91 +3,91 @@ using namespace std;
  
 class Stack{  //class for stack
 private:
-    int size;
-    int top;
-    int* S;
+    int size; //Max size of stack
+    int top; //The index of the top of the stack
+    int* data; //pointer to the array containing the stack
 public:
-    Stack(int size);
-    ~Stack();
-    void push(int x);
-    int pop();
-    int peek(int index);
-    int isFull();
-    int isEmpty();
-    void display();
-    int stackTop();
+    Stack(int size); //constructor for the stack that contains the int
+    ~Stack(); //destructor for the stack 
+    void push(int element); //function to push an element to the stack
+    int pop(); //removes the element from the stack
+    int peek(int index); //returns the element at the specified index
+    int isFull(); //to check if the stack is full
+    int isEmpty(); //to check if the stack is empty
+    void display(); //to display the stack
+    int stackTop(); //returns the element at top without removing it from the stack
 };
  
 Stack::Stack(int size) {
-    this->size = size;
-    top = -1;
-    S = new int[size];
+    this->size = size;  //can use this for methods declared as member functions. Assigning the size to stack
+    top = -1; //by default the top is -1. While returning empty stack it'll give error
+    data = new int[size];
 }
  
-Stack::~Stack() {
-    delete[] S;
+Stack::~Stack() {  //destructor that destructs the class struct
+    delete[] data; //deleting the stack data
 }
  
-void Stack::push(int x) {
+void Stack::push(int element) { //class method that pushes an int in the stack
     if (isFull()){
         cout << "Stack Overflow!" << endl;
     } else {
         top++;
-        S[top] = x;
+        data[top] = element;
     }
 }
  
-int Stack::pop() {
-    int x = 1;
+int Stack::pop() {  //class method that pops out an element from the stack
+    int element = 1;
     if (isEmpty()){
         cout << "Stack Underflow!" << endl;
     } else {
-        x = S[top];
+        element = data[top];
         top--;
     }
-    return x;
+    return element;
 }
  
-int Stack::peek(int index) {
-    int x = -1;
+int Stack::peek(int index) { //class method that tells the element at a given index
+    int element = -1;
     if (top-index+1 < 0 || top-index+1 == size){
         cout << "Invalid position!" << endl;
     } else {
-        x = S[top-index+1];
+        element = data[top-index+1];
     }
-    return x;
+    return element;
 }
  
-int Stack::isFull() {
+int Stack::isFull() { //class method to check if the stack is full
     if (top == size-1){
         return 1;
     }
     return 0;
 }
  
-int Stack::isEmpty() {
+int Stack::isEmpty() { //class method to check if the stack is empty
     if (top == -1){
         return 1;
     }
     return 0;
 }
  
-void Stack::display() {
+void Stack::display() { //class method to display the stack
     for (int i=top; i>=0; i--){
-        cout << S[i] << " | " << flush;
+        cout << data[i] << " | " << flush;
     }
     cout << endl;
 }
  
-int Stack::stackTop() {
+int Stack::stackTop() { //class method to display the top element of the stack
     if (isEmpty()){
         return -1;
     }
-    return S[top];
+    return data[top];
 }
  
  
-int main() {
+int main() { //main function
  
     int A[] = {1, 3, 5, 7, 9};
  
