d9ad2676fa0229654f2f23d1b491f7a681ed9694
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-05-25 20:38:11 +0300

    added LSD

--- classwork/Algorithms-class/tasks/Long-Long-LSD/working_LSD_sort.cpp
+++ classwork/Algorithms-class/tasks/Long-Long-LSD/working_LSD_sort.cpp
@@ -1,23 +1,17 @@
 //
 // Created by rituparn on 24.05.23.
 //
-
 #include <cstdint>
 #include <iostream>
-#include <vector>
 
 class LSDRadixSort {
-
-public:
-    long long *arr;
-    int size;
 public:
     static void Sort(long long* arr, int size) {
         const int kBits = 64;
         const int kRadix = 10;
 
         long long* temp[kRadix];
-        for (auto & i : temp) {
+        for (auto& i : temp) {
             i = new long long[size];
         }
 
@@ -37,24 +31,15 @@ public:
             }
         }
 
-        for (auto & i : temp) {
+        for (auto& i : temp) {
             delete[] i;
         }
-    }
-
-void Data(int num_inputs){
-        size=num_inputs;
-        arr= new long long [size];
-
-    for (int i = 0; i < size; ++i) {
-        std::cin>>arr[i];
-    }
     }
 };
 
 void PrintArray(const long long* arr, int size) {
     for (int i = 0; i < size; i++) {
-        std::cout << arr[i] <<"\n";
+        std::cout << arr[i] << "\n";
     }
 }
 
@@ -75,14 +60,3 @@ int main() {
 
     return 0;
 }
-
-/*
- * Static Member Function: When a member function is declared as static, it means that the function belongs to the
- * class itself rather than an instance of the class. Static member functions can be called using the class name
- * without creating an object of the class. They don't have access to the instance-specific data members and can
- * only access other static members of the class.
-
-    Static Member Variable: A static member variable is shared among all the instances of a class. It exists
-    independently of any class instance, and all the objects of the class share the same copy of the static member
-    variable. Static member variables must be defined outside the class declaration to allocate memory for them.
- */
